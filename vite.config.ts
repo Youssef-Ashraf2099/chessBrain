6fe3aa7313c8a2c0c47491cdc71e6d455afc0791
Youssef Ashraf <155368101+Youssef-Ashraf2099@users.noreply.github.com>
2026-02-11 19:23:51 +0200

    update compliance

--- vite.config.ts
+++ vite.config.ts
@@ -7,12 +7,16 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      injectRegister: 'auto',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg', 'sounds/*.mp3', 'movements/*.png'],
       manifest: {
         name: 'Chess Replay',
         short_name: 'Chess Replay',
         description: 'Chess Replay and Game Review for Youssef-2099',
-        theme_color: '#779556',
+        theme_color: '#00809dff',
+        background_color: '#1a2332',
+        display: 'standalone',
+        start_url: '/',
         icons: [
           {
             src: 'icons/icons8-chess-com-48.png',
@@ -25,8 +29,18 @@ export default defineConfig({
             type: 'image/png'
           },
           {
-            src: 'icons/icons8-chess-com-96.png',
-            sizes: '96x96',
+            src: 'icons/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/chess-512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/chess-512.png',
+            sizes: '512x512',
             type: 'image/png',
             purpose: 'any maskable'
           }
@@ -48,6 +62,9 @@ export default defineConfig({
           },
         ],
       },
+      devOptions: {
+        enabled: true
+      }
     })
   ],
 });
